4ea40764e075c14d69ff0e035741e507e39c9b53
Richard Badir <ricobadir@hotmail.com>
2024-04-11 19:10:47 -0400

    done all data extraction other than trending dates

--- retrieve_data.py
+++ retrieve_data.py
@@ -39,4 +39,65 @@ with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
     for row in results:
         writer.writerow(row)
 
+#TrackCreditors
+mycursor.execute("SELECT * FROM TrackCreditors")
+
+# Fetch all the results
+results = mycursor.fetchall()
+
+csv_file_path = 'track_creditors.csv'
+
+# Write the results to a CSV file
+with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["TrackID","CreditorID"])
+    for row in results:
+        writer.writerow(row)
+
+#ContentRating
+mycursor.execute("SELECT * FROM ContentRating")
+
+# Fetch all the results
+results = mycursor.fetchall()
+
+csv_file_path = 'content_rating.csv'
+
+# Write the results to a CSV file
+with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["contentRatingID","contentRatingName"])
+    for row in results:
+        writer.writerow(row)
+
+#TrackContentRating
+mycursor.execute("SELECT * FROM TrackContentRating")
+
+# Fetch all the results
+results = mycursor.fetchall()
+
+csv_file_path = 'track_content_rating.csv'
+
+# Write the results to a CSV file
+with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["TrackID","contentRatingID"])
+    for row in results:
+        writer.writerow(row)
+
+
+#TrackInsertionLog
+mycursor.execute("SELECT * FROM TrackInsertionLog")
+
+# Fetch all the results
+results = mycursor.fetchall()
+
+csv_file_path = 'track_insertion_log.csv'
+
+# Write the results to a CSV file
+with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["LogID","TrackID","TrackName"])
+    for row in results:
+        writer.writerow(row)
+
 mydb.close()
